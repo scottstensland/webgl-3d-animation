4ac9707951af204d64e0a23e857ee5a13bf426ec
Scott Stensland <scottstensland@gmail.com>
2014-08-01 19:58:20 -0400

    modified:   package.json 	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -1737,7 +1737,7 @@ console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
 console.log("process.env.SUBDOMAIN ", process.env.SUBDOMAIN);
 console.log("process.env.PORT ", process.env.PORT);
 
-console.log("version: 0.0.45   ");
+console.log("version: 0.0.46   ");
 
 var serviceUrl;
 var servicePort;
