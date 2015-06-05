55b875f220786a96a7d3eff107b4423b0bc4f19a
Scott Stensland <scottstensland@gmail.com>
2015-06-05 14:47:08 -0400

    modified:   schwartz_surface.js

--- src/schwartz_surface.js
+++ src/schwartz_surface.js
@@ -23,7 +23,17 @@ var SIZE_DIM_COLORS = 4;
 var FLT_MAX = 99999.99;
 var FLT_MIN = -99999.99;
 
-var animals_schwartz = {};
+var object_handle = {}; // holds all the various flavors of graphic object types
+
+var animals_schwartz  = "animals_schwartz";
+
+object_handle[animals_schwartz] = {};
+
+var all_object_labels = [];
+
+all_object_labels.push(animals_schwartz);
+
+
 
 // ------------
 
@@ -47,8 +57,8 @@ function init_schwartz(gl, given_max_num_schwartz_points) {
 
     // ----------------------
 
-    animals_schwartz.vertices = new Float32Array(given_max_num_schwartz_points * SIZE_DIM_3D);
-    animals_schwartz.colors   = new Float32Array(given_max_num_schwartz_points * SIZE_DIM_COLORS);
+    object_handle[animals_schwartz].vertices = new Float32Array(given_max_num_schwartz_points * SIZE_DIM_3D);
+    object_handle[animals_schwartz].colors   = new Float32Array(given_max_num_schwartz_points * SIZE_DIM_COLORS);
 
     var min_boundary = 0.0;
     // var max_boundary = 6.0;
@@ -71,28 +81,28 @@ function init_schwartz(gl, given_max_num_schwartz_points) {
 
     Common_Utils.init_min_max(min_max);
 
-    // var animals_schwartz = {};
+    // var object_handle[animals_schwartz] = {};
 
-    animals_schwartz.min_max = min_max;
+    object_handle[animals_schwartz].min_max = min_max;
 
-    animals_schwartz.want_translate = true;
-    // animals_schwartz.want_translate = false;
+    object_handle[animals_schwartz].want_translate = true;
+    // object_handle[animals_schwartz].want_translate = false;
 
-    // animals_schwartz.pre_translate  = [  1.5,  2.5,  1.5];
-    // animals_schwartz.pre_translate  = [  1.5,  2.5,  0.5];
-    // animals_schwartz.pre_translate  = [  1.5,  4.5,  0.5];
-    // animals_schwartz.pre_translate  = [  1.0,  4.5,  0.5];
-    // animals_schwartz.pre_translate  = [  1.0,  4.5,  0.0];
-    // animals_schwartz.pre_translate  = [  1.0,  3.5,  0.0];
+    // object_handle[animals_schwartz].pre_translate  = [  1.5,  2.5,  1.5];
+    // object_handle[animals_schwartz].pre_translate  = [  1.5,  2.5,  0.5];
+    // object_handle[animals_schwartz].pre_translate  = [  1.5,  4.5,  0.5];
+    // object_handle[animals_schwartz].pre_translate  = [  1.0,  4.5,  0.5];
+    // object_handle[animals_schwartz].pre_translate  = [  1.0,  4.5,  0.0];
+    // object_handle[animals_schwartz].pre_translate  = [  1.0,  3.5,  0.0];
 
-    // animals_schwartz.pre_translate  = [  2.0,  0.5,  -2.0];
-    // animals_schwartz.pre_translate  = [  0.0,  0.0,  0.0];
-    animals_schwartz.pre_translate  = [  0.0,  3.0,  0.0];
-    animals_schwartz.post_translate = Common_Utils.negate_array(animals_schwartz.pre_translate);
+    // object_handle[animals_schwartz].pre_translate  = [  2.0,  0.5,  -2.0];
+    // object_handle[animals_schwartz].pre_translate  = [  0.0,  0.0,  0.0];
+    object_handle[animals_schwartz].pre_translate  = [  0.0,  3.0,  0.0];
+    object_handle[animals_schwartz].post_translate = Common_Utils.negate_array(object_handle[animals_schwartz].pre_translate);
 
     var object_base_offset = -0.5;
 
-    animals_schwartz.rotation_array = [0.09, 0.08, -0.11];
+    object_handle[animals_schwartz].rotation_array = [0.09, 0.08, -0.11];
 
     var x;
     var y;
@@ -147,9 +157,9 @@ function init_schwartz(gl, given_max_num_schwartz_points) {
             // console.log('Z ', Z);
             // console.log('z ', z);
 
-            animals_schwartz.vertices[curr_borg_vertex*SIZE_DIM_3D + X] = x;
-            animals_schwartz.vertices[curr_borg_vertex*SIZE_DIM_3D + Y] = y;
-            animals_schwartz.vertices[curr_borg_vertex*SIZE_DIM_3D + Z] = z;
+            object_handle[animals_schwartz].vertices[curr_borg_vertex*SIZE_DIM_3D + X] = x;
+            object_handle[animals_schwartz].vertices[curr_borg_vertex*SIZE_DIM_3D + Y] = y;
+            object_handle[animals_schwartz].vertices[curr_borg_vertex*SIZE_DIM_3D + Z] = z;
 
             Common_Utils.populate_min_max(min_max, x, y, z);
 
@@ -162,63 +172,73 @@ function init_schwartz(gl, given_max_num_schwartz_points) {
 
     var diff_actual = min_max[max] - min_max[min];
 
-    var offset_borg_x = animals_schwartz.pre_translate[0] + object_base_offset;    // move into translation N rotation
-    var offset_borg_y = animals_schwartz.pre_translate[1] + object_base_offset;
-    var offset_borg_z = animals_schwartz.pre_translate[2] + object_base_offset;
+    var offset_borg_x = object_handle[animals_schwartz].pre_translate[0] + object_base_offset;    // move into translation N rotation
+    var offset_borg_y = object_handle[animals_schwartz].pre_translate[1] + object_base_offset;
+    var offset_borg_z = object_handle[animals_schwartz].pre_translate[2] + object_base_offset;
 
     for (var curr_normalize_point = 0; curr_normalize_point < curr_borg_vertex; curr_normalize_point++) {
 
-        animals_schwartz.colors[curr_borg_color_index + R] = (animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D +
+        object_handle[animals_schwartz].colors[curr_borg_color_index + R] = (object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D +
                                                                     X] / diff_actual);
-        animals_schwartz.colors[curr_borg_color_index + G] = (animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D +
+        object_handle[animals_schwartz].colors[curr_borg_color_index + G] = (object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D +
                                                                     Y] / diff_actual);
-        animals_schwartz.colors[curr_borg_color_index + B] = (animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D +
+        object_handle[animals_schwartz].colors[curr_borg_color_index + B] = (object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D +
                                                                     Z] / diff_actual);
-        animals_schwartz.colors[curr_borg_color_index + A] = 1.0;
+        object_handle[animals_schwartz].colors[curr_borg_color_index + A] = 1.0;
 
 
-        animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + X] = 
-        (animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + X] / diff_actual) + offset_borg_x;
+        object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + X] = 
+        (object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + X] / diff_actual) + offset_borg_x;
 
-        animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + Y] = 
-        (animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + Y] / diff_actual) + offset_borg_y;
+        object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + Y] = 
+        (object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + Y] / diff_actual) + offset_borg_y;
 
-        animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + Z] = 
-        (animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + Z] / diff_actual) + offset_borg_z;
+        object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + Z] = 
+        (object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + Z] / diff_actual) + offset_borg_z;
 
-        // console.log(animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + X],
-        //             animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + Y],
-        //             animals_schwartz.vertices[curr_normalize_point*SIZE_DIM_3D + Z]
+        // console.log(object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + X],
+        //             object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + Y],
+        //             object_handle[animals_schwartz].vertices[curr_normalize_point*SIZE_DIM_3D + Z]
         //                 );
 
 
         curr_borg_color_index += SIZE_DIM_COLORS;
     }
 
-    animals_schwartz.count = curr_borg_vertex;
+    object_handle[animals_schwartz].count = curr_borg_vertex;
 
-    animals_schwartz.vertex_position_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, animals_schwartz.vertex_position_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, animals_schwartz.vertices, gl.STATIC_DRAW);
-    animals_schwartz.vertex_position_buffer.itemSize = SIZE_DIM_3D;
-    animals_schwartz.vertex_position_buffer.numItems = curr_borg_vertex;
+    object_handle[animals_schwartz].vertex_position_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, object_handle[animals_schwartz].vertex_position_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, object_handle[animals_schwartz].vertices, gl.STATIC_DRAW);
+    object_handle[animals_schwartz].vertex_position_buffer.itemSize = SIZE_DIM_3D;
+    object_handle[animals_schwartz].vertex_position_buffer.numItems = curr_borg_vertex;
 
     // ---
 
-    animals_schwartz.vertex_color_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, animals_schwartz.vertex_color_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, animals_schwartz.colors, gl.STATIC_DRAW);
-    animals_schwartz.vertex_color_buffer.itemSize = SIZE_DIM_COLORS;
-    animals_schwartz.vertex_color_buffer.numItems = curr_borg_vertex;
+    object_handle[animals_schwartz].vertex_color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, object_handle[animals_schwartz].vertex_color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, object_handle[animals_schwartz].colors, gl.STATIC_DRAW);
+    object_handle[animals_schwartz].vertex_color_buffer.itemSize = SIZE_DIM_COLORS;
+    object_handle[animals_schwartz].vertex_color_buffer.numItems = curr_borg_vertex;
 
 }       //      init_borg
 
 
+var get_object_handle = function() {
+
+    return object_handle;
+}
+
+var get_all_object_labels = function() {
+
+    return all_object_labels;
+}
 
 return {    // to make visible to calling reference frame list function here
 
-  init_schwartz: init_schwartz,
-  animals_schwartz: animals_schwartz
+    init_schwartz         : init_schwartz,
+    get_object_handle     : get_object_handle,
+    get_all_object_labels : get_all_object_labels
 };
 
 }();    //  fns = function() 
