f9a5bf0e9edadc663670879cd02120355baa14ed
Scott Stensland <scottstensland@gmail.com>
2015-06-05 14:45:29 -0400

    modified:   borg.js

--- src/borg.js
+++ src/borg.js
@@ -23,7 +23,19 @@ var SIZE_DIM_COLORS = 4;
 var FLT_MAX = 99999.99;
 var FLT_MIN = -99999.99;
 
-var animals_borg = {};
+
+// ---
+
+var object_handle = {}; // holds all the various flavors of graphic object types
+
+var animals_borg  = "animals_borg";
+
+object_handle[animals_borg] = {};
+
+var all_object_labels = [];
+
+all_object_labels.push(animals_borg);
+
 
 // ------------
 
@@ -47,8 +59,8 @@ function init_borg(gl, given_max_borg_points) {
 
     // ----------------------
 
-    animals_borg.vertices = new Float32Array(given_max_borg_points * SIZE_DIM_3D);
-    animals_borg.colors   = new Float32Array(given_max_borg_points * SIZE_DIM_COLORS);
+    object_handle[animals_borg].vertices = new Float32Array(given_max_borg_points * SIZE_DIM_3D);
+    object_handle[animals_borg].colors   = new Float32Array(given_max_borg_points * SIZE_DIM_COLORS);
 
     var min_borg = 0.0;
     var max_borg = 6.0;
@@ -69,23 +81,23 @@ function init_borg(gl, given_max_borg_points) {
 
     Common_Utils.init_min_max(min_max);
 
-    // var animals_borg = {};
+    // var object_handle[animals_borg] = {};
 
-    animals_borg.min_max = min_max;
+    object_handle[animals_borg].min_max = min_max;
 
-    animals_borg.want_translate = true;
-    // animals_borg.want_translate = false;
+    object_handle[animals_borg].want_translate = true;
+    // object_handle[animals_borg].want_translate = false;
 
-    animals_borg.pre_translate  = [  2.5,  3.5,  0.5];
-    // animals_borg.pre_translate  = [  0.5,  2.5,  0.5];
-    animals_borg.post_translate = Common_Utils.negate_array(animals_borg.pre_translate);
-    // animals_borg.post_translate[Z] += 2.0;
+    object_handle[animals_borg].pre_translate  = [  2.5,  3.5,  0.5];
+    // object_handle[animals_borg].pre_translate  = [  0.5,  2.5,  0.5];
+    object_handle[animals_borg].post_translate = Common_Utils.negate_array(object_handle[animals_borg].pre_translate);
+    // object_handle[animals_borg].post_translate[Z] += 2.0;
 
-    // console.log('animals_borg.post_translate ', vec3.str(animals_borg.post_translate));
+    // console.log('object_handle[animals_borg].post_translate ', vec3.str(object_handle[animals_borg].post_translate));
 
 
-    // animals_borg.rotation_array = [-0.09, -0.08, 0.11];
-    animals_borg.rotation_array = [ 0.69, -0.8, 2.11];
+    // object_handle[animals_borg].rotation_array = [-0.09, -0.08, 0.11];
+    object_handle[animals_borg].rotation_array = [ 0.69, -0.8, 2.11];
 
     var borg_blob = {};
 
@@ -143,9 +155,9 @@ function init_borg(gl, given_max_borg_points) {
             // console.log('Z ', Z);
             // console.log('z ', z);
 
-            animals_borg.vertices[curr_borg_vertex*SIZE_DIM_3D + X] = x;
-            animals_borg.vertices[curr_borg_vertex*SIZE_DIM_3D + Y] = y;
-            animals_borg.vertices[curr_borg_vertex*SIZE_DIM_3D + Z] = z;
+            object_handle[animals_borg].vertices[curr_borg_vertex*SIZE_DIM_3D + X] = x;
+            object_handle[animals_borg].vertices[curr_borg_vertex*SIZE_DIM_3D + Y] = y;
+            object_handle[animals_borg].vertices[curr_borg_vertex*SIZE_DIM_3D + Z] = z;
 
             Common_Utils.populate_min_max(min_max, x, y, z);
 
@@ -159,64 +171,74 @@ function init_borg(gl, given_max_borg_points) {
     var diff_actual = min_max[max] - min_max[min];
 
     var object_base_offset =  -0.5;
-    var offset_borg_x = animals_borg.pre_translate[0] + object_base_offset;    // move into translation N rotation
-    var offset_borg_y = animals_borg.pre_translate[1] + object_base_offset;
-    var offset_borg_z = animals_borg.pre_translate[2] + object_base_offset;
+    var offset_borg_x = object_handle[animals_borg].pre_translate[0] + object_base_offset;    // move into translation N rotation
+    var offset_borg_y = object_handle[animals_borg].pre_translate[1] + object_base_offset;
+    var offset_borg_z = object_handle[animals_borg].pre_translate[2] + object_base_offset;
 
-    // var offset_borg_x = animals_borg.pre_translate[0] + borg_blob.translate_x;    // move into translation N rotation
-    // var offset_borg_y = animals_borg.pre_translate[1] + borg_blob.translate_y;
-    // var offset_borg_z = animals_borg.pre_translate[2] + borg_blob.translate_z;
+    // var offset_borg_x = object_handle[animals_borg].pre_translate[0] + borg_blob.translate_x;    // move into translation N rotation
+    // var offset_borg_y = object_handle[animals_borg].pre_translate[1] + borg_blob.translate_y;
+    // var offset_borg_z = object_handle[animals_borg].pre_translate[2] + borg_blob.translate_z;
 
     // var color_smasher = 0.4;
     var color_smasher = 1.0;
 
     for (var curr_normalize_point = 0; curr_normalize_point < curr_borg_vertex; curr_normalize_point++) {
 
-        animals_borg.colors[curr_borg_color_index + R] = color_smasher * (animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D +
+        object_handle[animals_borg].colors[curr_borg_color_index + R] = color_smasher * (object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D +
                                                                     X] / diff_actual);
-        animals_borg.colors[curr_borg_color_index + G] = color_smasher * (animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D +
+        object_handle[animals_borg].colors[curr_borg_color_index + G] = color_smasher * (object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D +
                                                                     Y] / diff_actual);
-        animals_borg.colors[curr_borg_color_index + B] = color_smasher * (animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D +
+        object_handle[animals_borg].colors[curr_borg_color_index + B] = color_smasher * (object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D +
                                                                     Z] / diff_actual);
-        animals_borg.colors[curr_borg_color_index + A] = 1.0;
+        object_handle[animals_borg].colors[curr_borg_color_index + A] = 1.0;
 
 
-        animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D + X] = 
-        (animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D + X] / diff_actual) + offset_borg_x;
+        object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D + X] = 
+        (object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D + X] / diff_actual) + offset_borg_x;
 
-        animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D + Y] = 
-        (animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D + Y] / diff_actual) + offset_borg_y;
+        object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D + Y] = 
+        (object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D + Y] / diff_actual) + offset_borg_y;
 
-        animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D + Z] = 
-        (animals_borg.vertices[curr_normalize_point*SIZE_DIM_3D + Z] / diff_actual) + offset_borg_z;
+        object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D + Z] = 
+        (object_handle[animals_borg].vertices[curr_normalize_point*SIZE_DIM_3D + Z] / diff_actual) + offset_borg_z;
 
         curr_borg_color_index += SIZE_DIM_COLORS;
     }
 
-    animals_borg.count = curr_borg_vertex;
+    object_handle[animals_borg].count = curr_borg_vertex;
 
-    animals_borg.vertex_position_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, animals_borg.vertex_position_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, animals_borg.vertices, gl.STATIC_DRAW);
-    animals_borg.vertex_position_buffer.itemSize = SIZE_DIM_3D;
-    animals_borg.vertex_position_buffer.numItems = curr_borg_vertex;
+    object_handle[animals_borg].vertex_position_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, object_handle[animals_borg].vertex_position_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, object_handle[animals_borg].vertices, gl.STATIC_DRAW);
+    object_handle[animals_borg].vertex_position_buffer.itemSize = SIZE_DIM_3D;
+    object_handle[animals_borg].vertex_position_buffer.numItems = curr_borg_vertex;
 
     // ---
 
-    animals_borg.vertex_color_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, animals_borg.vertex_color_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, animals_borg.colors, gl.STATIC_DRAW);
-    animals_borg.vertex_color_buffer.itemSize = SIZE_DIM_COLORS;
-    animals_borg.vertex_color_buffer.numItems = curr_borg_vertex;
+    object_handle[animals_borg].vertex_color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, object_handle[animals_borg].vertex_color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, object_handle[animals_borg].colors, gl.STATIC_DRAW);
+    object_handle[animals_borg].vertex_color_buffer.itemSize = SIZE_DIM_COLORS;
+    object_handle[animals_borg].vertex_color_buffer.numItems = curr_borg_vertex;
 
 }       //      init_borg
 
 
+var get_object_handle = function() {
+
+    return object_handle;
+}
+
+var get_all_object_labels = function() {
+
+    return all_object_labels;
+}
 
 return {    // to make visible to calling reference frame list function here
 
-  init_borg: init_borg,
-  animals_borg: animals_borg
+    init_borg             : init_borg,
+    get_object_handle     : get_object_handle,
+    get_all_object_labels : get_all_object_labels
 };
 
 }();    //  fns = function() 
